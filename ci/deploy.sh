b97206fa89a7e3be6069e59bae343cadc919135e
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-12 14:57:14 -0600

    Use Castulos config

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -98,9 +98,23 @@ ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
 ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IP 'chown stack:stack /opt/stack/'
 ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IP 'cd /opt/stack; sudo -H -u stack git clone https://git.openstack.org/openstack-dev/grenade'
 ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IP 'sudo -H -u stack cat <<EOT >> /opt/stack/grenade/devstack.localrc
-# Additional devstack configuration goes here.
-disable_all_services
-enable_service key mysql
+# Disable heat.
+disable_service h-api h-api-cfn h-api-cw h-eng heat
+
+# Speed things up by tuning down screen sleep time.
+SCREEN_SLEEP=0.5
+
+# Switch to neutron.
+disable_service n-net
+enable_service q-agt q-dhcp q-l3 q-meta q-svc quantum
+
+Q_USE_DEBUG_COMMAND=True
+NETWORK_GATEWAY=192.168.0.1
+FIXED_RANGE=192.168.0.0/20
+FLOATING_RANGE=172.24.5.0/24
+PUBLIC_NETWORK_GATEWAY=172.24.5.1
+
+USE_SCREEN=False
 EOT'
 ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IP "cd /opt/stack/grenade; sudo -H -u stack git checkout $GRENADE_BRANCH"
 ssh -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IP 'cd /opt/stack/grenade; sudo -H -u stack ./grenade.sh'
