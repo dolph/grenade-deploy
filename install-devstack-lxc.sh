973784c3552639b469c7a4cb5b8c0abc449f6869
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-19 14:18:56 -0600

    Run tempest smoke tests

--- install-devstack-lxc.sh
+++ install-devstack-lxc.sh
@@ -8,8 +8,16 @@ done
 apt-get install -y git
 adduser --disabled-password --gecos "" stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-cd /home/stack;
+
+cd /home/stack
 sudo -H -u stack git clone https://github.com/osic/devstack-lxc.git
-cd /home/stack/devstack-lxc;
+cd /home/stack/devstack-lxc
 sudo -H -u stack sudo ./install-multinode.sh
+
+cd /home/stack
+sudo -H -u stack git clone https://github.com/openstack/tempest.git
+cd /home/stack/tempest
+tox -esmoke
+
+cd /home/stack/devstack-lxc
 sudo -H -u stack sudo ./teardown.sh
