426f55f88d0db03e05f6a3204c3201d29914f93d
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-19 15:49:10 -0600

    Install additional deps

--- install-devstack-lxc.sh
+++ install-devstack-lxc.sh
@@ -5,7 +5,7 @@ for i in `seq 1 10`;
 do
     apt-get update && break || sleep 15
 done
-apt-get install -y git build-essential python-dev python-tox
+apt-get install -y git
 adduser --disabled-password --gecos "" stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
@@ -14,10 +14,13 @@ sudo -H -u stack git clone https://github.com/osic/devstack-lxc.git
 cd /home/stack/devstack-lxc
 sudo -H -u stack sudo ./install-multinode.sh
 
+# Smoke test
+apt-get install -y build-essential python-dev python-tox libxml2-dev libxslt-dev
 cd /home/stack
 sudo -H -u stack git clone https://github.com/openstack/tempest.git
 cd /home/stack/tempest
 tox -esmoke
 
+# Tear down
 cd /home/stack/devstack-lxc
 sudo -H -u stack sudo ./teardown.sh
