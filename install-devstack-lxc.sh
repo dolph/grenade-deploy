74d77d26ccceb5a4a50980203e278f826966c94b
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-19 08:26:26 -0600

    Use /home/stack

--- install-devstack-lxc.sh
+++ install-devstack-lxc.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -ex
 
-GRENADE_BRANCH=$1
-
 apt-get update
 apt-get install -y git
 adduser --disabled-password --gecos "" stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-mkdir -p /opt/stack/
-chown stack:stack /opt/stack/
-cd /opt/stack; sudo -H -u stack git clone https://github.com/osic/devstack-lxc.git
-cd /opt/stack/devstack-lxc; sudo -H -u stack ./install-multinode.sh
-cd /opt/stack/devstack-lxc; sudo -H -u stack ./teardown.sh
+cd /home/stack;
+sudo -H -u stack git clone https://github.com/osic/devstack-lxc.git
+cd /home/stack/devstack-lxc;
+sudo -H -u stack ./install-multinode.sh
+sudo -H -u stack ./teardown.sh
