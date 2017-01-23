8ef691db9d957cbf186e6c873b95db1844be6412
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-23 11:43:54 -0600

    Change branches

--- install-devstack-lxc.sh
+++ install-devstack-lxc.sh
@@ -14,6 +14,7 @@ echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 cd /home/stack
 sudo -H -u stack git clone https://github.com/osic/devstack-lxc.git
 cd /home/stack/devstack-lxc
+git checkout lxc-multinode-grenade
 sudo -H -u stack sudo ./install-multinode.sh
 
 # Smoke test
