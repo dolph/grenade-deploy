648cb424c84270243dd60d894f67bf76a14f1b53
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-26 13:46:37 -0600

    Drop branch option

--- install-osa.sh
+++ install-osa.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-BRANCH=${1:-master}
-
 PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
 for i in `seq 1 10`;
@@ -12,7 +10,6 @@ done
 apt-get install -y git
 
 cd /opt/openstack-ansible
-git checkout $BRANCH
 export BOOTSTRAP_OPTS="bootstrap_host_ubuntu_repo=http://mirror.rackspace.com/ubuntu"
 export ANSIBLE_ROLE_FETCH_MODE=git-clone
 bash scripts/bootstrap-ansible.sh
