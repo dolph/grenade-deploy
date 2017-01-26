51615e62e4f31a284b97e623c65f08ba39bc1dd1
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-26 13:48:23 -0600

    Add debug output

--- install-osa.sh
+++ install-osa.sh
@@ -13,5 +13,8 @@ cd /opt/openstack-ansible
 export BOOTSTRAP_OPTS="bootstrap_host_ubuntu_repo=http://mirror.rackspace.com/ubuntu"
 export ANSIBLE_ROLE_FETCH_MODE=git-clone
 bash scripts/bootstrap-ansible.sh
+echo Bootstrap Ansible: $?
 bash scripts/bootstrap-aio.sh
+echo Bootstrap AIO: $?
 bash scripts/run-playbooks.sh
+echo Run playbooks: $?
