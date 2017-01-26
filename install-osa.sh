bbaa0fc09f26bbeb0c88b733261d9294266a625d
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-26 13:49:36 -0600

    Do not install git (unused)

--- install-osa.sh
+++ install-osa.sh
@@ -5,7 +5,6 @@ for i in `seq 1 10`;
 do
     apt-get update && break || sleep 15
 done
-apt-get install -y git
 
 cd /opt/openstack-ansible
 export BOOTSTRAP_OPTS="bootstrap_host_ubuntu_repo=http://mirror.rackspace.com/ubuntu"
