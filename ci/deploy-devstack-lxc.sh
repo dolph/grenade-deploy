21ff4449abbad07ae99634cef7674132249acd32
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-03 15:06:49 -0600

    Use a consistent instance name

--- ci/deploy-devstack-lxc.sh
+++ ci/deploy-devstack-lxc.sh
@@ -8,7 +8,7 @@ RACK_API_KEY=$4
 RACK_REGION=$5
 IMAGE_NAME=$6
 
-INSTANCE_NAME="ci-devstack-lxc-`shuf -i 100000-999999 -n 1`"
+INSTANCE_NAME="ci-devstack-lxc"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -17,7 +17,7 @@ source $DIR/common-functions.sh
 bootstrap
 bootstrap_ssh "$SSH_PUBLIC_KEY" "$SSH_PRIVATE_KEY_BODY"
 bootstrap_rack "$RACK_USERNAME" "$RACK_API_KEY" "$RACK_REGION"
-trap "delete_instance \"$INSTANCE_NAME\"" EXIT
+delete_instance "$INSTANCE_NAME"
 provision_instance "$INSTANCE_NAME" "$IMAGE_NAME" "8 GB Performance"
 
 public_ip=$(get_public_ip $INSTANCE_NAME)
