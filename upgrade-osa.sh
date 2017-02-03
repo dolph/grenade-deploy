61a5e2d257ec5ef481c13990d5b177f7e91c55f4
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-03 12:58:32 -0600

    Upgrade to latest tag

--- upgrade-osa.sh
+++ upgrade-osa.sh
@@ -2,6 +2,10 @@
 set -ex
 
 cd /opt/openstack-ansible
+
+# Find the latest tag.
 git checkout master
+git checkout `git describe --abbrev=0`
+
 export I_REALLY_KNOW_WHAT_I_AM_DOING=true
 bash scripts/run-upgrade.sh
