fdcf3e3d5524f6b517606475b743a561bdd1a0f5
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-06 10:57:11 -0600

    Add additional deps

--- ci/deploy-rally.sh
+++ ci/deploy-rally.sh
@@ -8,8 +8,14 @@ source $DIR/common-functions.sh
 bootstrap
 
 apt-get install -y \
-    wget \
     git \
+    iputils-ping \
+    libffi-dev \
+    libpq-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxslt1-dev \
     python-pip \
+    wget \
     ;
 bash rally/install_rally.sh
