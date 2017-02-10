246499617af4e6c0758c38cb55a297699798b7f8
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-10 07:55:30 -0600

    Install python-dev

--- install-keystone.sh
+++ install-keystone.sh
@@ -6,7 +6,7 @@ do
     apt-get update && break || sleep 15
 done
 
-apt-get install -y tox libpq-dev
+apt-get install -y tox python-dev libpq-dev
 
 cd /opt/openstack-ansible-os_keystone
 tox -e upgrade
