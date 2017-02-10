f7c2ef303ede40d7a49ab563b71b5aea2f93af1a
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-10 10:22:00 -0600

    Refactor

--- install-keystone.sh
+++ install-keystone.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ex
 
+export DEBIAN_FRONTEND=noninteractive
+
 for i in `seq 1 10`;
 do
     apt-get update && break || sleep 15
 done
 
-# Ensure python 2.7 is installed.
-apt-get install -y \
-    python2.7 \
-    ;
+# Ensure python 2.7 is installed, because python3 is the default.
+apt-get install -y python2.7
 
 # Install pip from source.
 curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py \
@@ -18,8 +18,9 @@ curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py \
 cd /opt/openstack-ansible-os_keystone
 
 # Install bindep to discover binary deps.
-pip install bindep tox
-export DEBIAN_FRONTEND=noninteractive
+pip install bindep
 apt-get install -y $(bindep -b -f bindep.txt test || true)
 
+# Test upgrades.
+pip install tox
 tox -e upgrade
