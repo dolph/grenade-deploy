19810d6120649460ac42744ec8b350a4869d1fa9
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-10 08:51:25 -0600

    Run bootstrap-ansible first

--- install-keystone.sh
+++ install-keystone.sh
@@ -13,5 +13,10 @@ apt-get install -y tox \
     libpq-dev \
     ;
 
+cd /opt/openstack-ansible
+export BOOTSTRAP_OPTS="bootstrap_host_ubuntu_repo=http://mirror.rackspace.com/ubuntu"
+export ANSIBLE_ROLE_FETCH_MODE=git-clone
+bash scripts/bootstrap-ansible.sh
+
 cd /opt/openstack-ansible-os_keystone
 tox -e upgrade
