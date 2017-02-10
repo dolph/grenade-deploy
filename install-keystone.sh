42ac99b6e63cab3366a159699c0002fda17603b5
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-10 08:15:56 -0600

    Add openssl header files

--- install-keystone.sh
+++ install-keystone.sh
@@ -6,7 +6,11 @@ do
     apt-get update && break || sleep 15
 done
 
-apt-get install -y tox python-dev libpq-dev
+apt-get install -y tox \
+    python-dev \
+    libssl-dev \
+    libpq-dev \
+    ;
 
 cd /opt/openstack-ansible-os_keystone
 tox -e upgrade
